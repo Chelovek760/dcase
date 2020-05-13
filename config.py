b17042a2b211c01912a2e5fb165589ae5bee1c39
Chelovek760 <Chelovek760@gmail.com>
2020-05-13 22:42:26 +0300

    v 0.668

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 import os
 
-TEST_WAV_DIR = r'D:\Ботать\Работа\dcase\dev_data\fan\test'
-TRAIN_WAV_DIR = r'D:\Ботать\Работа\dcase\dev_data\fan\train'
-TEST_PIC_DIR = r'D:\Ботать\Работа\dg.a.core\data\graph\dcase\27.04' + '\\'
+TEST_WAV_DIR = r'D:\Projects\dcase\dev_data\fan\test'
+TRAIN_WAV_DIR = r'D:\Projects\dcase\dev_data\fan\train'
 DCASE_JSON_DIR = 'D:\\Ботать\\Работа\\dg.a.core\\data\\json\\dcase\\fan\\train\\'
-DCASE_CSV_DIR_TEST = r'D:\Ботать\Работа\dcase\thrash\csv_vgg19_features\test' + '\\'
-DCASE_CSV_DIR_TRAIN = r'D:\Ботать\Работа\dcase\thrash\csv_vgg19_features\train' + '\\'
+DCASE_CSV_DIR_TEST = r'D:\Projects\dcase\thrash\csv_vggish_features\test' + '\\'
+DCASE_CSV_DIR_TRAIN = r'D:\Projects\dcase\thrash\csv_vggish_features\train' + '\\'
 BAD_CASE_WAV = TEST_WAV_DIR + r'segment_data\bad\\'
 GOOD_CASE_WAV = TEST_WAV_DIR + r'segment_data\good\\'
